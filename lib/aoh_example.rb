0786fee68cf8262658433c369335b51ced7313b5
Chana Kupetz <chanakupetz@gmail.com>
2020-07-31 04:15:46 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,7 +22,8 @@ def literal_aoh
     { name:  "Kate Gompert", occupation: "None" }
     { name:  "Bruce Green", occupation: "Fan of Mildred" }
   ]
-
+end
+names
 end
 
 def aoh_lookup(aoh, row, key)
